f9da24b75e12258e75f346ff2c8a52848af346b6
Sneha <snehajad@cybage.com>
2022-03-29 11:45:25 +0530

    post table is created

--- shout_backend/database/migrations/2022_03_28_130053_create_posts_table.php
+++ shout_backend/database/migrations/2022_03_28_130053_create_posts_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            // $table->timestamp('last_used_at')->nullable();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('postId');
+            $table->date('postDate');
+            $table->string('postContent');
+            $table->string('postedUserId');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('posts');
     }
 };
