6eb58b2f0ecc1e391dff0ce8b66e4cd2e655fe7e
Sneha <snehajad@cybage.com>
2022-03-30 08:30:35 +0530

    controller is created

--- shout_backend/app/Http/Controllers/PostController.php
+++ shout_backend/app/Http/Controllers/PostController.php
@@ -16,5 +16,29 @@ class PostController extends Controller
     // {
     //     return Post::create($request->all());
     // }
+    
+  
+    public function destroy($id)
+    {
+        return post::destroy($id);
+    }
+    public function search($name){
+        return post::where('name', 'like', '%'.$name.'%')->get();
+    }
+    public function create(){
+        $photos = post::all();
+        return view('post',compact('photos'));
+    }
+    public function store(Request $request){
+        // $size=$request->file('postContent')->getSize();
+        $name=$request->file('postContent')->getClientOriginalName();
+        return $name;
+        $request->file('postContent')->storeAs('public/images/',$name);
+        $post = new Post();
+        $post->name = $name;
+        // $post->size = $size;
+        $post->save();
+        return redirect()->back();
+    }
 
 }
