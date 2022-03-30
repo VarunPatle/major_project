232fc66ff3de1ab35e79b4dec0eadb0221ac02f8
Sneha <snehajad@cybage.com>
2022-03-30 09:09:35 +0530

    route is created

--- shout_backend/routes/api.php
+++ shout_backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\FriendsController;
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\ReportsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +18,31 @@ use App\Http\Controllers\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('users',UserController::class);
+Route::resource('user',UserController::class);
+// Route::resource('post',PostController::class);
+Route::resource('comments',CommentsController::class);
+Route::resource('friends',FriendsController::class);
+Route::resource('reports',ReportsController::class);
 
-Route::get('/users/search/{name}', [UserController::class, 'search']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
+Route::delete('user/{id}',[UserController::class,'destroy']);
+Route::delete('post/{id}',[PostController::class,'destroy']);
+Route::delete('friends/{id}',[FriendsController::class,'destroy']);
+Route::delete('comments/{id}',[CommentsController::class,'destroy']);
+
+
+
+
+Route::get('/user/search/{name}', [UserController::class, 'search']);
+Route::get('/comments/search/{id}', [CommentsController::class, 'search']);
+
+
+
+// // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// //     return $request->user();
+// });
+// Route::middleware('auth:sanctum')->get('/comments', function (Request $request) {
+    // return $request->comments();
+// });
 
