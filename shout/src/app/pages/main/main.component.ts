68a81cc052b0ea53c2e0061ae01ba17988d89a0a
Varun <varunpa@cybage.com>
2022-04-07 09:26:53 +0530

    working on search user

--- shout/src/app/pages/main/main.component.ts
+++ shout/src/app/pages/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, NgForm, Validators, FormBuilder} from '@angular/forms'
 import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,17 +12,22 @@ export class MainComponent implements OnInit {
   posts: any;
   post:any;
   user:any;
+  searchedUser:any;
   username:string;
-  constructor(private postService: PostService) { }
+  loginForm: FormGroup;
+  constructor(private userService: UserService, private formBuilder:FormBuilder) {
+    
+  }
 
   ngOnInit(): void {
     this.user = JSON.parse(sessionStorage.user);
     this.username = this.user.name;
   }
-  getUsersData(){
-    // this.postService.getPosts().subscribe(res => {
-    //   this.posts = res;
-    // })
+  searchUser(){
+    console.log();
+    this.userService.searchUser(this.searchedUser).subscribe(res => {
+      this.searchedUser = res;
+    });
   }
 
 
--- shout/src/app/services/user.service.ts
+++ shout/src/app/services/user.service.ts
@@ -23,6 +23,8 @@ export class UserService {
     return this.httpClient.post<any>(this._loginUrl, data);
   }
 
-
+  searchUser(name:string){
+    return this.httpClient.get('http://localhost:8000/api/users/search/'+name);
+  }
 
 }
